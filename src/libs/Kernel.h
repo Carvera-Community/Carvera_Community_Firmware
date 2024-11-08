d766894f7467e37a2fc32a3ba61720ea9f981203
ACaesuraIsStillMusic <faecorrigandesign@gmail.com>
2024-11-08 13:13:23 -0800

    Revert "Merge pull request #39 from Carvera-Community/main-staging"
    This reverts commit 9d8e0ae96735f06ae423bbe673f7ab0378ba5993, reversing
    changes made to 5a76137539b693c899e0c427df87bd1d738e57e7.

--- src/libs/Kernel.h
+++ src/libs/Kernel.h
@@ -93,7 +93,6 @@ typedef struct {
 	float TOOLMZ;
 	float reserve;
 	int TOOL;
-    float perm_vars[20];
 } EEPROM_data;
 
 class Kernel {
@@ -114,8 +113,6 @@ class Kernel {
 
         bool kernel_has_event(_EVENT_ENUM id_event, Module *module);
         void unregister_for_event(_EVENT_ENUM id_event, Module *module);
-
-        float get_user_var(int var_num);
 
         bool is_using_leds() const { return use_leds; }
         bool is_halted() const { return halted; }
@@ -192,7 +189,6 @@ class Kernel {
         uint8_t halt_reason;
         uint8_t atc_state;
         EEPROM_data *eeprom_data;
-        float local_vars[20];
 
     private:
         // When a module asks to be called for a specific event ( a hook ), this is where that request is remembered
