abf464296e7f0c47b81a0e9c9b094b745e0d6cb2
MFlego <9705612+MFlego@users.noreply.github.com>
2026-08-11 13:49:10 -0400

    - applied exact header text in compensation preprocessor files - active-comp blocking logic ( G91 and G5x) are enforced in one place and G54-G59 and G91 to the active-comp blocked G-code set.  (still working on this to make it better) - Added query/status reporting for cutter compensation for future controller interaction and gets for info - Build is green (unteseted, will test at the end)

--- src/modules/robot/CompensationTypes.h
+++ src/modules/robot/CompensationTypes.h
@@ -1,24 +1,17 @@
 /*
-      This file is part of Smoothie (http://smoothieware.org/).
-      Smoothie is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-      Smoothie is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-      You should have received a copy of the GNU General Public License along with Smoothie. If not, see <http://www.gnu.org/licenses/>.
+This file is part of Carvera Community Firmware, a fork of Smoothieware ([http://smoothieware.org/](http://smoothieware.org/)).
+Carvera Community Firmware is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+Carvera Community Firmware is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with Carvera Community Firmware. If not, see [http://www.gnu.org/licenses/](http://www.gnu.org/licenses/).
 */
 
 #ifndef COMPENSATION_TYPES_H
 #define COMPENSATION_TYPES_H
 
-/**
- * Cutter Compensation Types
- * Corresponds to G-code commands:
- * - G40: Compensation Off
- * - G41: Compensation Left (tool offset to left of path)
- * - G42: Compensation Right (tool offset to right of path)
- */
 enum class CompensationType {
-    NONE = 0,   // G40 - No compensation
-    LEFT = 1,   // G41 - Tool offset to left
-    RIGHT = 2   // G42 - Tool offset to right
+    NONE = 0,   // G40 - canel compensation
+    LEFT = 1,   // G41 - Tool offset left
+    RIGHT = 2   // G42 - Tool offset right
 };
 
 #endif // COMPENSATION_TYPES_H
